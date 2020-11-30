56bb1f070157bf4fe5e589d9b0e06dda3412605b
Anonimas1 <puzinas.audrius@gmail.com>
2020-11-30 22:50:13 +0200

    Add database seeder

--- ITprojektas/database/seeders/DatabaseSeeder.php
+++ ITprojektas/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +14,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('animal_genders')->truncate();
+        DB::table('animal_genders')->insert([
+            'name' => 'berniukas'
+        ]);
+        DB::table('animal_genders')->insert([
+            'name' => 'mergaite'
+        ]);
+        DB::table('animal_states')->truncate();
+        DB::table('animal_states')->insert([
+            'name' => 'dingęs'
+        ]);
+        DB::table('animal_states')->insert([
+            'name' => 'rastas'
+        ]);
     }
 }
