7ae3966a6d8360451fa49ac6c7bdf1f9c97a2fc1
Anonimas1 <puzinas.audrius@gmail.com>
2020-11-30 22:31:11 +0200

    Make migrations for database

---
+++ ITprojektas/database/migrations/2020_11_30_195458_create_animals_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_path');
+            $table->text('description');
+            $table->integer('age');
+            $table->string('home_address');
+            $table->unsignedBigInteger('state');
+            $table->unsignedBigInteger('gender');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+
+            $table->foreign('state')->references('id')->on('animal_states');
+            $table->foreign('gender')->references('id')->on('animal_genders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
