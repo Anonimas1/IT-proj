8ef5b5b5f9b550b4ad0e6cfdc9af05e37163ee89
Anonimas1 <puzinas.audrius@gmail.com>
2020-12-01 13:54:43 +0200

    Enable user seeding

--- ITprojektas/database/seeders/DatabaseSeeder.php
+++ ITprojektas/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('animal_genders')->truncate();
         DB::table('animal_genders')->insert([
