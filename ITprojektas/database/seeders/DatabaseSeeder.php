b340bb1bc74f057ba21065bc59f2f7c2a08d17f0
Anonimas1 <puzinas.audrius@gmail.com>
2020-12-03 16:16:52 +0200

    Add tables, seeders required for auth

--- ITprojektas/database/seeders/DatabaseSeeder.php
+++ ITprojektas/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
 
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('animal_genders')->truncate();
@@ -30,5 +29,35 @@ class DatabaseSeeder extends Seeder
         DB::table('animal_states')->insert([
             'name' => 'rastas'
         ]);
+
+
+        $permissions = ['view-post', 'create-post', 'create-comment', 'view-comment', 'delete-post', 'see-post-views', 'change-user-permissions'];
+        DB::table('permissions')->truncate();
+        foreach($permissions as $perm){
+            DB::table('permissions')->insert([
+                'name' => $perm
+            ]);
+        }
+
+        $roles = ['unregistered', 'registered', 'moderator', 'admin'];
+        DB::table('roles')->truncate();
+        foreach($roles as $role){
+            DB::table('roles')->insert([
+                'name' => $role
+            ]);
+        }
+        //Number of permisions for each role
+        $permissions_roles = [1, 4, 6, 7];
+        DB::table('permission_role')->truncate();
+        for($i = 1; $i <= 4; $i++){
+            for($j=1; $j <=$permissions_roles[$i - 1]; $j++){
+                DB::table('permission_role')->insert([
+                    'role_id' => $i,
+                    'permission_id' => $j
+                ]);
+            }
+        }
+        DB::table('users')->truncate();
+        \App\Models\User::factory(10)->create();
     }
 }
